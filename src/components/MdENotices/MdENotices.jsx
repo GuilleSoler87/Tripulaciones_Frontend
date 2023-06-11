bee03f0c151f59873978097d77e1c047982d4930
GuileSoler87 <guillermosoler87@gmail.com>
2023-06-11 05:22:15 +0200

    Home navigation & menu

---
+++ src/components/MdENotices/MdENotices.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./MdENotices.scss";
+
+const MdENotices = () => {
+  return (
+    <div>MdENotices</div>
+  )
+}
+
+export default MdENotices
---
+++ src/components/TabNotices/TabNotices.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./TabNotices.scss";
+
+const TabNotices = () => {
+  return (
+    <div>TabNotices</div>
+  )
+}
+
+export default TabNotices
