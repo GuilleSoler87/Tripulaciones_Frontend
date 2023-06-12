fb3854ed6f1ff5d9d8e9e9ff0f7fcbc9ecf9b5b6
pacool1234 <niederleytnerenciso@gmail.com>
2023-06-12 21:29:48 +0200

    updated style of ChatList

--- src/components/ChatList/ChatList.jsx
+++ src/components/ChatList/ChatList.jsx
@@ -4,36 +4,36 @@ import "./ChatList.scss";
 import { UserContext } from "../../context/UserContext/UserState";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import points from "../../../src/images/points_upAll.png";
+import groupChats from "../../../src/images/groupal_chats.png";
 
 const ChatList = () => {
   const { user, chats, getUser, getChatsFromUser } = useContext(UserContext);
   const navigate = useNavigate();
-  // const [receiver, setReceiver] = useState(null);
+  const [isSingle, setIsSingle] = useState(true);
 
   useEffect(() => {
-    getUser(),
-    getChatsFromUser();
-  })
+    getUser(), getChatsFromUser();
+  });
 
-  const API_URL = "http://localhost:8080/"
+  const API_URL = "http://localhost:8080/";
 
-  
   const getReceiver = (chat) => {
     const usersArray = chat.users;
-    const receiver = usersArray.find(x => x.username !== user.username);
+    const receiver = usersArray.find((x) => x.username !== user.username);
     return receiver;
-  }
+  };
 
   const extractFilePathFromImage = (path) => {
-    const url = "https://desafio-backend-production.up.railway.app"; //CHANGE to pertinent URL
+    const url = "https://desafio-backend-production.up.railway.app/"; //CHANGE to pertinent URL
     // const url = "http://localhost:8080/"; //CHANGE to pertinent URL
     try {
       const match = path.match(/uploads[\\\/](.+)/);
-      return match ? url + match[1].replace(/\\/g, '/') : null;
+      return match ? url + match[1].replace(/\\/g, "/") : null;
     } catch {
       return "../../src/images/no_pic.png";
     }
-  }
+  };
 
   const formatTime = (utcDate) => {
     let date = new Date(utcDate);
@@ -47,16 +47,30 @@ const ChatList = () => {
     let yesterday = new Date();
     yesterday.setDate(today.getDate() - 1);
     if (date.toDateString() === yesterday.toDateString()) {
-        return "Ayer";
+      return "Ayer";
     }
 
     var lastMonday = new Date();
     lastMonday.setDate(today.getDate() - ((today.getDay() + 6) % 7));
     if (date > lastMonday) {
-        return ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'][date.getDay() - 1];
+      return [
+        "Lunes",
+        "Martes",
+        "Miércoles",
+        "Jueves",
+        "Viernes",
+        "Sábado",
+        "Domingo",
+      ][date.getDay() - 1];
     }
 
-    return ('0' + date.getDate()).slice(-2) + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' + date.getFullYear();
+    return (
+      ("0" + date.getDate()).slice(-2) +
+      "/" +
+      ("0" + (date.getMonth() + 1)).slice(-2) +
+      "/" +
+      date.getFullYear()
+    );
   };
 
   const formatLastMessage = (message) => {
@@ -65,25 +79,42 @@ const ChatList = () => {
       return message.slice(0, maxLength) + "...";
     }
     return message;
-  }
+  };
 
-  const sender = { ...user };
+  const groupChatImage = (
+    <img src={groupChats}></img>
+  )
 
   const chatList = chats.map((chat, index) => {
     return (
-      <div key={index} className="chatItem" onClick={() => navigate(`/chat/${chat._id}`)}>
+      <div
+        key={index}
+        className="chatItem"
+        onClick={() => navigate(`/chat/${chat._id}`)}
+      >
         <div className="imageDiv">
           <div className="imageContainer">
-            <img src={extractFilePathFromImage(getReceiver(chat).img) ? extractFilePathFromImage(getReceiver(chat).img) : "/uploads/users/no_pic.png"} width={150} />
+            <img
+              src={
+                extractFilePathFromImage(getReceiver(chat).img)
+                  ? extractFilePathFromImage(getReceiver(chat).img)
+                  : "/uploads/users/no_pic.png"
+              }
+              width={150}
+            />
           </div>
         </div>
         <div className="chatInfo">
           <div className="namePlusDate">
             <p className="leftUsername">{getReceiver(chat).username}</p>
-            <p className="rightTime">{formatTime(chat.history[chat.history.length-1].date)}</p>
+            <p className="rightTime">
+              {formatTime(chat.history[chat.history.length - 1].date)}
+            </p>
           </div>
           <div className="firstRowOfMessage">
-            <p>{formatLastMessage(chat.history[chat.history.length-1].message)}</p>
+            <p>
+              {formatLastMessage(chat.history[chat.history.length - 1].message)}
+            </p>
           </div>
         </div>
       </div>
@@ -93,11 +124,20 @@ const ChatList = () => {
   return (
     <>
       <Header />
-      <div className="chatListTitleContainer">
-        <h1>Chat</h1>
-      </div>
-      <div className="chatListDiv">
-        {chatList}
+      <div className="chatListContainer">
+        <div className="chatListTitleContainer">
+          <p>Chat</p>
+          <img className="pointsImage" src={points} />
+        </div>
+        <div className="switchingButtonsContainer">
+          <button className="single" onClick={() => setIsSingle(true)}>
+            Individuales
+          </button>
+          <button className="group" onClick={() => setIsSingle(false)}>
+            Grupales
+          </button>
+        </div>
+        <div className="chatListDiv">{isSingle ? chatList : groupChatImage}</div>
       </div>
       <Footer />
     </>
