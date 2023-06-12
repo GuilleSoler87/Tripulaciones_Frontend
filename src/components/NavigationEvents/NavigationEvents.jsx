00ac5dc9a496949eef4075efbd3a375951d2e907
joanbaldo <joanbaldo@hotmail.com>
2023-06-12 14:07:11 +0200

    EVENTS hell side scroll raw-view SOLVED

--- src/components/NavigationEvents/NavigationEvents.jsx
+++ src/components/NavigationEvents/NavigationEvents.jsx
@@ -4,31 +4,34 @@ import "./NavigationEvents.scss";
 const NavigationEvents = () => {
   return (
     <>
-    
-    <div className='main_container_navigation_events'>
 
+      <div className='main_container_navigation_events'>
 
-        <div>
-
+        <div className='eventRecomendFixed'>
         </div>
 
+        <div className='eventSideScroll'>
 
-
-        <div>
-            
+          <div className='eventSideText'>
+            <p className='sideText'>Recomendados para ti</p>
+          </div>
+          <div className='eventSideArray'>
+            <div className='eventside1'>
+            </div>
+            <div className='eventside2'>
+            </div>
+            <div className='eventside3'>
+            </div>
+          </div>
         </div>
 
-
-
-
-        <div>
-            
+        <div className='eventList_Down'>
         </div>
+      </div>
 
 
-        
-    </div>   
-    
+
+
     </>
   )
 }
