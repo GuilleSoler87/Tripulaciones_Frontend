a5b80d2041e437f2fbc1cbd99dadfeddc69970aa
joanbaldo <joanbaldo@hotmail.com>
2023-06-12 22:45:57 +0200

    WIP EVENTS view

--- src/components/EventSingle/EventSingle.jsx
+++ src/components/EventSingle/EventSingle.jsx
@@ -7,18 +7,30 @@ import Footer from '../Footer/Footer';
 const EventSingle = () => {
   return (
     <>
-    <Header/>
+      <Header />
 
 
-   <div className='singleEventMain'>
+      <div className='singleEventMain'>
+
+        <div className='eventPic'></div>
+
+        <div className='eventPicFooter'>
+          <p className='picFooterText'>Auditorio Paco Pons - EDEM</p>
+          <div className='picFooterIcon'>
+            <img src="./src/images/icon_share.png" alt="Compartir" />
+          </div>
+        </div>
+
+        <div className='dotsOverlay'>
+            <img src="./src/images/points_horWhite.png" alt="Puntos" />
+
+        </div>
+
+      </div>
 
 
+      <Footer />
 
-   </div>
-
-
-   <Footer />
-    
     </>
   )
 }
